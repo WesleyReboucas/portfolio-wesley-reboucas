e886c9ac005e459f9f78e30ec25e10d914581858
WesleyReboucas <wesley.reboucas@gmail.com>
2024-03-13 14:54:41 -0300

    fix(layout): position of SpeedInsights and Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
       <body>
         <StyledComponentsRegistry>
           <GlobalStyle />
+          {children}
           <SpeedInsights />
           <Analytics />
-          {children}
         </StyledComponentsRegistry>
       </body>
     </html>
